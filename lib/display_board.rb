7b850a1921c8d306c0b79c187db2d2ff647c274b
Jennifer Hong <jennifer.hong13@gmail.com>
2018-06-06 20:32:00 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,4 @@
-def capture_puts
-    board = [" "," "," "," "," "," "," "," "," "]
-end
+board = [" "," "," "," "," "," "," "," "," "]
 
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
@@ -9,5 +7,3 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
-
-display_board(board)
